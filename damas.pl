156d633bb05ad474ba0acc642f803a783a054a15
humbbetao <humberto_voleibol@hotmail.com>
2016-06-24 02:21:43 -0300

    funciona

--- damas.pl
+++ damas.pl
@@ -1,33 +1,82 @@
-comerPeca("9C", "8D", "E0").
-row(M, N, Row) :-
-    nth1(N, M, Row).
+casa("1A",1,1,0).
+casa("1B",1,1,0).
+casa("1C",1,1,0).
+casa("1D",1,1,0).
+casa("1E",1,1,0).
+casa("1F",1,1,0).
+casa("1G",1,1,0).
+casa("1H",1,1,0).
 
-column(M, N, Col) :-
-    transpose(M, MT),
-    row(MT, N, Col).
+casa("2A",1,1,0).
+casa("2B",1,1,0).
+casa("2C",1,1,0).
+casa("2D",1,1,0).
+casa("2E",1,1,0).
+casa("2F",1,1,0).
+casa("2G",1,1,0).
+casa("2H",1,1,0).
 
-symmetrical(M) :-
-    transpose(M, M).
+casa("3A",1,1,0).
+casa("3B",1,1,0).
+casa("3C",1,1,0).
+casa("3D",1,1,0).
+casa("3E",1,1,0).
+casa("3F",1,1,0).
+casa("3G",1,1,0).
+casa("3H",1,1,0).
 
-transpose([[]|_], []) :- !.
-transpose([[I|Is]|Rs], [Col|MT]) :-
-    first_column([[I|Is]|Rs], Col, [Is|NRs]),
-    transpose([Is|NRs], MT).
+casa("4A",1,1,0).
+casa("4B",1,1,0).
+casa("4C",1,1,0).
+casa("4D",1,1,0).
+casa("4E",1,1,0).
+casa("4F",1,1,0).
+casa("4G",1,1,0).
+casa("4H",1,1,0).
 
-first_column([], [], []).
-first_column([[]|_], [], []).
-first_column([[I|Is]|Rs], [I|Col], [Is|Rest]) :-
-    first_column(Rs, Col, Rest).
+casa("5A",1,1,0).
+casa("5B",1,1,0).
+casa("5C",1,1,0).
+casa("5D",1,1,0).
+casa("5E",1,1,0).
+casa("5F",1,1,0).
+casa("5G",1,1,0).
+casa("5H",1,1,0).
 
+casa("6A",1,1,0).
+casa("6B",1,1,0).
+casa("6C",1,1,0).
+casa("6D",1,1,0).
+casa("6E",1,1,0).
+casa("6F",1,1,0).
+casa("6G",1,1,0).
+casa("6H",1,1,0).
 
-imprimir( [ ] ).
-imprimir( [ X | Y ] ) :- nl, write(X), imprimir( Y ).
+casa("7A",1,1,0).
+casa("7B",1,1,0).
+casa("7C",1,1,0).
+casa("7D",1,1,0).
+casa("7E",1,1,0).
+casa("7F",1,1,0).
+casa("7G",1,1,0).
+casa("7H",1,1,0).
 
+casa("8A",1,1,0).
+casa("8B",1,1,0).
+casa("8C",1,1,0).
+casa("8D",1,1,0).
+casa("8E",1,1,0).
+casa("8F",1,1,0).
+casa("8G",1,1,0).
+casa("8H",1,1,0).
 
+imprimir( [ ], I, J ).
+imprimir( [ X | Y ], I, J ) :-	I is 1, J is 1, write('    1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 '), nl, write(I), write(' - '),write(X), F is I+1, G is J+1, imprimir( Y, F, G );
+								I\=1 , J is 1, write(I), write(' - '), write(X), F is I+1, G is J+1, imprimir( Y, F, G );
+								I\=1, J\=1, write(' | '), write(X), F is I+1, G is J+1, imprimir( Y, F, G ).
 
-imprimirMat( [ ] ).
-imprimirMat( [ X | Y ] ) :- imprimirMat( X ), nl, write(X), imprimirMat(Y).
-
+imprimirMat( [ ] , I, J).
+imprimirMat( [ X | Y ], I, J ) :- imprimir( X, I, J),nl, F is I+1 ,imprimirMat(Y, F, J	).
 
 
 matriz([["_","B","_","B","_","B","_","B"],["B","_","B","_","B","_","B","_"],	["_","B","_","B","_","B","_","B"],["1","_","1","_","1","_","1","_"],["_","1","_","1","_","1","_","1"],["P","_","P","_","P","_","P","_"],["_","P","_","P","_","P","_","P"],["P","_","P","_","P","_","P","_"]]).
@@ -38,11 +87,27 @@ writeLinha(M):-imprimir(M).
 
 criarMatrizInicial(M):- writeLinha(M).
 
-vencedor(M, V, T):- turnar(M, T)
+verificarMovimento(M, I, J, X, Y):- write('Deu certo').
+%verificarMovimento(M, I, J, X, Y):- verificarij(M, I, J), verificarxy(M, X, Y), K is X-I, L is Y-J, not(k,1), not(L,1), moverPeca(M, )
+
+turnodojogador(M, I, J, X, Y):-write('Jogo Atual ') ,nl, imprimirMat(M, 1, 1), nl,
+								write('Digite a linha onde esta'), nl, read(I), 
+								write('Digite a coluna onde esta'), nl, read(J),
+								write('Digite a linha onde vai'), nl, read(X),
+								write('Digite a coluna onde vai'), nl, read(Y),
+								verificarMovimento(M, I, J, X, Y).
+								
+
+mod2(T, NumeroDoTurno):- NumeroDoTurno is mod(T,2).
+
+turnar(M,T):- mod2(T, NumeroDoTurno), NumeroDoTurno is 0, turnodojogador(M, I, J, X, Y).
+%turnar(M,T):- mod2(T, NumeroDoTurno), Y is 0, turnodojogador(M, I, J, X, Y); mod2(T, Y), Y is 1, turnodocomputador(M).
+
+vencedor(M, V, T):- turnar(M, T).
 
 printarVencedor(V):- printe(v,"Jogador") ; printe(V, "computador").
 
-damas(M):- vencedor(M, V, 0), printarVencedor(T).
+damas(M):- vencedor(M, V, 0), printarvencedor(T).
 
 %turnar(M, turno)
 
